558a3993e00f5974d5b0d200e67bff350b6e4805
zj <379881811@qq.com>
2017-04-08 23:12:42 +0800

    增加状态查询

--- logistics/src/main/java/com/bs/tenement/query/OrderQuery.java
+++ logistics/src/main/java/com/bs/tenement/query/OrderQuery.java
@@ -3,6 +3,8 @@ package com.bs.tenement.query;
 public class OrderQuery extends Query{
 
 	private String yType;
+	
+	private Integer status;
 
 	public String getyType() {
 		return yType;
@@ -11,6 +13,13 @@ public class OrderQuery extends Query{
 	public void setyType(String yType) {
 		this.yType = yType;
 	}
-	
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
 	
 }
